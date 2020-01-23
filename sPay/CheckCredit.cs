1d6297499ffd2de2c591d2cbec8d064adcd37758
Viktoras Kazbaras <viktoras@vs.lt>
2020-01-24 01:44:46 +0200

    ok

---
+++ sPay/CheckCredit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SkiaSharp;
+
+namespace sPay
+{
+    [Activity(Label = "ChekCredit")]
+    public class CheckCredit : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.CheckCreditLay);
+            ImageView imageView1 = FindViewById<ImageView>(Resource.Id.imageView1);
+            ImageView imageView2 = FindViewById<ImageView>(Resource.Id.imageView2);
+            imageView1.SetImageResource(Resource.Drawable.credit_card_outline);
+            // Create your application here
+
+        }
+    }
+}
---
+++ sPay/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace sPay
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.main);
+            ImageButton imageButton1 = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            ImageButton imageButton2 = FindViewById<ImageButton>(Resource.Id.imageButton2);
+            ImageButton imageButton3 = FindViewById<ImageButton>(Resource.Id.imageButton3);
+            imageButton1.SetBackgroundResource(Resource.Drawable.credit_card_outline);
+            imageButton2.SetBackgroundResource(Resource.Drawable.shopping_outline);
+            imageButton3.SetBackgroundResource(Resource.Drawable.history);
+            imageButton1.Click += ImageButton1_Click;
+            //imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+            //avd = AnimatedVectorDrawableCompat.Create(this, Resource.Drawable.avd_feed);
+            //avd.RegisterAnimationCallback(new AnimatedCallback(this, imageView, avd));
+        }
+
+        private void ImageButton1_Click(object sender, System.EventArgs e)
+        {
+            //Toast.MakeText(this, "Helo", ToastLength.Short).Show();
+            Intent intent = new Intent(this, typeof(CheckCredit));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
